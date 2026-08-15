4eeae96c5a261345723f6650b951bfc0791e9f7f
Sarath G <sarathkarakkad1@gmail.com>
2026-08-15 21:03:30 +0530

    Update API_URL to new Apps Script deployment (Version 11)

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbyfxExlnko_FWoFWTGDnJkiU0NRFbx1Mu6a40DR8o1Fl3OCYbImVr4-vau2AxGFDpsS3A/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbw5pJWGFYGzIlp3ryNCZMM2_sB1G3q_CkC653vEcWT5XejJMyvnyJgWNpvKhrCQQgj6gQ/exec',
   RAZORPAY_KEY_ID: 'rzp_live_TPADM2b83cSu8Q'
 };
